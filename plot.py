f599bdc14028b9d5bdc2d464c93498b1955aaaae
William H. Grover <wgrover@gmail.com>
2022-05-22 12:37:23 -0700

    now marking missing data

--- plot.py
+++ plot.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import os
+from tabnanny import check
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def url():
-    plt.gcf().text(0.13, 0.85, "https://wgrover.github.io/RUSD-COVID", fontsize=8)
+    plt.gcf().text(0.045, 0.88, "https://wgrover.github.io/RUSD-COVID", fontsize=8)
     plt.gcf().text(0.87, 0.02, "2022")
 
 dates = []
@@ -14,54 +15,80 @@ elementarys = []
 middles = []
 highs = []
 others = []
+missed_dates = []
 
 files = os.listdir("./data")
 files.sort()
-print("\t\tElem\tMidd\tHigh\tOther\tSchools")
+
+checked_dates = []
 
 for filename in files:
     if ".DS_Store" in filename:
         continue
-    print(filename[:10], end="\t")
-    dates.append(datetime.strptime(filename[:10], "%Y-%m-%d"))
-    infile = open("./data/" + filename, "r")
-    elementary = 0
-    middle = 0
-    high = 0
-    other = 0
-    for line in infile:
-        # print(line, end="")
-        id, cat, count, name = line.strip().split("\t")
-        count = int(count)
-        if cat == "E":
-            elementary += count
-        elif cat == "M":
-            middle += count
-        elif cat == "H":
-            high += count
-        elif cat == "O":
-            other += count
-    print(f"{elementary}\t{middle}\t{high}\t{other}\t{elementary+middle+high}")
-    elementarys.append(elementary)
-    middles.append(middle)
-    highs.append(high)
-    others.append(other)
+    checked_dates.append(datetime.strptime(filename[:10], "%Y-%m-%d"))
+
+print("\t\tElem\tMidd\tHigh\tOther\tSchools")
+
+first_day = checked_dates[0]
+print("\t\tFIRSTDAY ", first_day.strftime("%m/%d/%Y"))
+
+last_day = checked_dates[-1]
+print("\t\tLASTDAY ", last_day.strftime("%m/%d/%Y"))
+
+num_days = (last_day - first_day).days + 1
+
+all_dates = [first_day + timedelta(days=x) for x in range(num_days)]
+
+for date in all_dates:
+    print(date.strftime("%m/%d/%Y"), end="\t")
+    if date in checked_dates:
+        dates.append(date)
+        infile = open("./data/" + date.strftime("%Y-%m-%d") + ".txt", "r")
+        elementary = 0
+        middle = 0
+        high = 0
+        other = 0
+        for line in infile:
+            id, cat, count, name = line.strip().split("\t")
+            count = int(count)
+            if cat == "E":
+                elementary += count
+            elif cat == "M":
+                middle += count
+            elif cat == "H":
+                high += count
+            elif cat == "O":
+                other += count
+        print(f"{elementary}\t{middle}\t{high}\t{other}\t{elementary+middle+high}")
+        elementarys.append(elementary)
+        middles.append(middle)
+        highs.append(high)
+        others.append(other)
+    else:
+        print("MISSED")
+        missed_dates.append(date)
+
 
 schools = numpy.array(elementarys) + numpy.array(middles) + numpy.array(highs)
 
-plt.figure(figsize=(12, 4))
+plt.figure(figsize=(12, 4), dpi=300)
 
 plt.cla()
 plt.bar(dates, schools, color="k")
+span = plt.gca().get_ylim()[1] - plt.gca().get_ylim()[0]
+plt.plot(missed_dates, [0.02*span] * len(missed_dates), "ro", markersize=3)
 plt.title("Active cases at all RUSD schools (14-day rolling window)")
 url()
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%b %e"))
 for label in plt.gca().get_xticklabels(which='major'):
     label.set(rotation=30, horizontalalignment='right')
+plt.tight_layout()
 plt.savefig("all_schools.png")
 
 plt.cla()
 plt.bar(dates, elementarys, color="b")
+span = plt.gca().get_ylim()[1] - plt.gca().get_ylim()[0]
+plt.plot(missed_dates, [0.02*span] * len(missed_dates), "ro", markersize=3)
 plt.title("Active cases at elementary schools (14-day rolling window)")
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%b %e"))
 for label in plt.gca().get_xticklabels(which='major'):
@@ -69,7 +96,9 @@ for label in plt.gca().get_xticklabels(which='major'):
 plt.savefig("elementary_schools.png")
 
 plt.cla()
-plt.bar(dates, middles, color="orange")
+plt.bar(dates, middles, color="green")
+span = plt.gca().get_ylim()[1] - plt.gca().get_ylim()[0]
+plt.plot(missed_dates, [0.02*span] * len(missed_dates), "ro", markersize=3)
 plt.title("Active cases at middle schools (14-day rolling window)")
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%b %e"))
 for label in plt.gca().get_xticklabels(which='major'):
@@ -77,7 +106,9 @@ for label in plt.gca().get_xticklabels(which='major'):
 plt.savefig("middle_schools.png")
 
 plt.cla()
-plt.bar(dates, highs, color="r")
+plt.bar(dates, highs, color="orange")
+span = plt.gca().get_ylim()[1] - plt.gca().get_ylim()[0]
+plt.plot(missed_dates, [0.02*span] * len(missed_dates), "ro", markersize=3)
 plt.title("Active cases at high schools (14-day rolling window)")
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%b %e"))
 for label in plt.gca().get_xticklabels(which='major'):
@@ -86,6 +117,8 @@ plt.savefig("high_schools.png")
 
 plt.cla()
 plt.bar(dates, others, color="0.80")
+span = plt.gca().get_ylim()[1] - plt.gca().get_ylim()[0]
+plt.plot(missed_dates, [0.02*span] * len(missed_dates), "ro", markersize=3)
 plt.title("Active non-school cases (14-day rolling window)")
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%b %e"))
 for label in plt.gca().get_xticklabels(which='major'):
